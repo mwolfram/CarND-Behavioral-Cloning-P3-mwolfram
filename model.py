49245ef5adb0f74f3814a0d169aaa3d0589e21f4
Mate Wolfram <mate.wolfram@gmail.com>
2017-03-25 15:01:03 +0100

    feature maps are generated, network is not too good

--- model.py
+++ model.py
@@ -12,30 +12,62 @@ from random import shuffle
 import sklearn
 from sklearn.model_selection import train_test_split
 import zipfile
+import matplotlib as mpl
+mpl.use('Agg') # run headless
+import matplotlib.pyplot as plt
 
-RUN_TARGET_FLOYD = "floyd"
-run_target_str = sys.argv[0]
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Convolution2D, MaxPooling2D, Dropout
+from keras.regularizers import l2
 
 # configuration
-if run_target_str is RUN_TARGET_FLOYD:
-  DATA_FOLDER = "/input/" # for floyd
-else:
-  DATA_FOLDER = "../CarND-Behavioral-Cloning-P3-data/multiple_data/" # on local machine
-
+USE_FLOYD = False
+DO_TRAIN = False
 SIDE_IMAGE_STEERING_BIAS = 0.2
 VALIDATION_SPLIT = 0.2
 BATCH_SIZE = 32
+PREDICT_IMAGES = ["sample_data/center_2016_12_01_13_31_13_177.jpg", "t2_forward_data/center_2017_03_22_17_03_16_764.jpg"]
 
+if USE_FLOYD:
+  # running on floydhub
+  DATA_FOLDER = "/input/" 
+  DATASETS = ["t1_reverse_data.csv", "t1_udacity_data.csv", "t2_forward_data.csv"]
+  EPOCHS = 7
+  OUTPUT_FOLDER = "/output/"
+else:
+  # running on local machine
+  DATA_FOLDER = "../CarND-Behavioral-Cloning-P3-data/multiple_data/" 
+  DATASETS = ["sample_data.csv"]
+  EPOCHS = 2
+  OUTPUT_FOLDER = ""
 
-def getCSVLines(csvfilename):
+# =========================================
+# Toolkit
+
+# reads lines from a csv file, skipping the first line
+def getCSVLines(csvpath):
   lines = []
-  with open(DATA_FOLDER + csvfilename) as csvfile:
+  with open(csvpath) as csvfile:
     reader = csv.reader(csvfile)
     iterlines = iter(reader)
     next(iterlines)
     for line in iterlines:
       lines.append(line)
   return lines
+
+# reads lines from all given datasets and concatenates them
+def getCSVLinesFromDatasets(data_folder, datasets):
+  lines = []
+  for entry in datasets:
+    lines.extend(getCSVLines(data_folder + entry))
+  return lines
+
+# reads an image from a zip file
+def getImageFromZip(zipname, filename):
+  zipped_images = zipfile.ZipFile(DATA_FOLDER + zipname + ".zip")
+  imagedata = zipped_images.read(filename)
+  image = cv2.imdecode(np.frombuffer(imagedata, np.uint8), 1)
+  return image
 
 # reads images and measurements from samples (csv file lines)
 def readImagesAndMeasurements(samples, augment=True):
@@ -77,6 +109,7 @@ def readImagesAndMeasurements(samples, augment=True):
 
   return images, measurements
 
+# generates shuffled data from given samples
 def generator(samples, batch_size=BATCH_SIZE):
   num_samples = len(samples)
   while 1: # Loop forever so the generator never terminates
@@ -90,81 +123,216 @@ def generator(samples, batch_size=BATCH_SIZE):
       y_train = np.array(angles)
       yield sklearn.utils.shuffle(X_train, y_train)
 
+def getBasicModel():
+  model = Sequential()
+  model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+  model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+  model.add(Flatten())
+  model.add(Dense(1))
+
+  # choose loss function and optimizer, compile the model
+  model.compile(loss='mse', optimizer='adam')
+
+  return model
+
+def getLeNetModel():
+  model = Sequential()
+  model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+  model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+  model.add(Convolution2D(6, 5, 5, activation="relu"))
+  model.add(MaxPooling2D())
+  model.add(Convolution2D(6, 5, 5, activation="relu"))
+  model.add(MaxPooling2D())
+  model.add(Flatten())
+  model.add(Dense(120))
+  model.add(Dense(84))
+  model.add(Dense(1))
+  
+  # choose loss function and optimizer, compile the model
+  model.compile(loss='mse', optimizer='adam')
+  return model
+
+def getNVIDIAModel():
+  model = Sequential()
+  model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+  model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+  model.add(Convolution2D(10, 1, 1, activation="relu", W_regularizer=l2(0.01))) # let the model choose the best color space
+  model.add(Dropout(0.5))
+  model.add(Convolution2D(3 , 1, 1, activation="relu", W_regularizer=l2(0.01))) # --"--
+  model.add(Dropout(0.5))
+  model.add(Convolution2D(24, 5, 5, subsample=(2, 2),  activation="relu", W_regularizer=l2(0.01)))
+  model.add(Dropout(0.5))
+  model.add(Convolution2D(36, 5, 5, subsample=(2, 2),  activation="relu", W_regularizer=l2(0.01)))
+  model.add(Dropout(0.5))
+  model.add(Convolution2D(48, 5, 5, subsample=(2, 2),  activation="relu", W_regularizer=l2(0.01)))
+  model.add(Dropout(0.5))
+  model.add(Convolution2D(64, 3, 3, activation="relu", W_regularizer=l2(0.01)))
+  model.add(Dropout(0.5))
+  model.add(Convolution2D(64, 3, 3, activation="relu", W_regularizer=l2(0.01)))
+  model.add(Flatten())
+  model.add(Dense(100, W_regularizer=l2(0.01)))
+  model.add(Dropout(0.5))
+  model.add(Dense(50, W_regularizer=l2(0.01)))
+  model.add(Dropout(0.5))
+  model.add(Dense(10, W_regularizer=l2(0.01)))
+  model.add(Dropout(0.5))
+  model.add(Dense(1, W_regularizer=l2(0.01)))
+
+  # choose loss function and optimizer, compile the model
+  model.compile(loss='mse', optimizer='adam')
+  return model
+
+def getPartialNVIDIAModelAndSetWeights(original_model_with_weights, depth=0):
+  model = original_model_with_weights
+
+  # Construct a second model for visualizing the activation
+  model2 = Sequential()
+  model2.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+  model2.add(Cropping2D(cropping=((70, 25), (0, 0))))
+  model2.add(Convolution2D(10, 1, 1, activation="relu", W_regularizer=l2(0.01), weights=model.layers[2].get_weights())) # let the model choose the best color space
+  if depth <= 0: 
+    model2.compile(loss='mse', optimizer='adam')
+    return model2
+  model2.add(Dropout(0.5))
+  model2.add(Convolution2D(3 , 1, 1, activation="relu", W_regularizer=l2(0.01), weights=model.layers[4].get_weights())) # --"--
+  if depth is 1: 
+    model2.compile(loss='mse', optimizer='adam')
+    return model2
+  model2.add(Dropout(0.5))
+  model2.add(Convolution2D(24, 5, 5, subsample=(2, 2),  activation="relu", W_regularizer=l2(0.01), weights=model.layers[6].get_weights()))
+  if depth is 2: 
+    model2.compile(loss='mse', optimizer='adam')
+    return model2
+  model2.add(Dropout(0.5))
+  model2.add(Convolution2D(36, 5, 5, subsample=(2, 2),  activation="relu", W_regularizer=l2(0.01), weights=model.layers[8].get_weights()))
+  if depth is 3: 
+    model2.compile(loss='mse', optimizer='adam')
+    return model2
+  model2.add(Dropout(0.5))
+  model2.add(Convolution2D(48, 5, 5, subsample=(2, 2),  activation="relu", W_regularizer=l2(0.01), weights=model.layers[10].get_weights()))
+  if depth is 4: 
+    model2.compile(loss='mse', optimizer='adam')
+    return model2
+  model2.add(Dropout(0.5))
+  model2.add(Convolution2D(64, 3, 3, activation="relu", W_regularizer=l2(0.01), weights=model.layers[12].get_weights()))
+  if depth is 5: 
+    model2.compile(loss='mse', optimizer='adam')
+    return model2
+  model2.add(Dropout(0.5))
+  model2.add(Convolution2D(64, 3, 3, activation="relu", W_regularizer=l2(0.01), weights=model.layers[14].get_weights()))
+  if depth >= 6: 
+    model2.compile(loss='mse', optimizer='adam')
+    return model2
+  model2.add(Flatten())
+  model2.add(Dense(100, W_regularizer=l2(0.01)))
+  model2.add(Dropout(0.5))
+  model2.add(Dense(50, W_regularizer=l2(0.01)))
+  model2.add(Dropout(0.5))
+  model2.add(Dense(10, W_regularizer=l2(0.01)))
+  model2.add(Dropout(0.5))
+  model2.add(Dense(1, W_regularizer=l2(0.01)))
+
+  model2.compile(loss='mse', optimizer='adam')
+  return model2
+
+def saveHistoryPlot(history_object):
+  plt.plot(history_object.history['loss'])
+  plt.plot(history_object.history['val_loss'])
+  plt.title('model mean squared error loss')
+  plt.ylabel('mean squared error loss')
+  plt.xlabel('epoch')
+  plt.legend(['training set', 'validation set'], loc='upper right')
+  plt.show()
+
+  plt.savefig(OUTPUT_FOLDER + 'history.png')
+
+  plt.cla()   # Clear axis
+  plt.clf()   # Clear figure
+  plt.close() # Close a figure window
+
+def train_model(model, samples):
+  # create generators for training and validation sets
+  train_samples, validation_samples = train_test_split(samples, test_size=VALIDATION_SPLIT)
+  train_generator = generator(train_samples, batch_size=BATCH_SIZE)
+  validation_generator = generator(validation_samples, batch_size=BATCH_SIZE)
+
+  # fit model with generator
+  history_object = model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=EPOCHS)
+  
+  return history_object
+
+# visualizes activation maps and saves these images under target_file
+def outputFeatureMap(activation, target_file, activation_min=-1, activation_max=-1 ,plt_num=1):
+  featuremaps = activation.shape[3]
+  fig = plt.figure(plt_num, figsize=(15,15))
+  for featuremap in range(featuremaps):
+    plot = fig.add_subplot(6,8, featuremap+1) # sets the number of feature maps to show on each row and column
+    #plt.title('FeatureMap ' + str(featuremap)) # displays the feature map number
+    if activation_min != -1 & activation_max != -1:
+      plot.imshow(activation[0,:,:, featuremap], interpolation="nearest", vmin =activation_min, vmax=activation_max, cmap="gray")
+    elif activation_max != -1:
+      plot.imshow(activation[0,:,:, featuremap], interpolation="nearest", vmax=activation_max, cmap="gray")
+    elif activation_min !=-1:
+      plot.imshow(activation[0,:,:, featuremap], interpolation="nearest", vmin=activation_min, cmap="gray")
+    else:
+      plot.imshow(activation[0,:,:, featuremap], interpolation="nearest", cmap="gray")
+  fig.savefig(OUTPUT_FOLDER + target_file)
+  plt.cla()   # Clear axis
+  plt.clf()   # Clear figure
+  plt.close() # Close a figure window
+  fig.clf()
+
+def getActivationMapFromImage(model, dataset_name, filename):
+  image_array = np.asarray(getImageFromZip(dataset_name, filename))
+  activation = model.predict(image_array[None, :, :, :], batch_size=1)
+  return activation
+
+# generates an activation map for each image in the list.
+# an image is addressed as <dataset_name>/<filename>
+# the activation maps are generated for several network depths
+def generateActivationMapsForImages(images, depths, target_folder="feature_maps/"):
+
+  for image in images:
+    dataset_name = image.split('/')[0].strip()
+    filename = image.split('/')[1].strip()
+
+    original_image = getImageFromZip(dataset_name, filename)
+    cv2.imwrite(OUTPUT_FOLDER + target_folder + dataset_name + "_" + filename + "_original.png", original_image)
+
+    for depth in range(depths):
+      # get a partial model with the weights set from the original model
+      partial_model = getPartialNVIDIAModelAndSetWeights(model, depth)
+
+      # get activation map from one image
+      activation = getActivationMapFromImage(partial_model, dataset_name, filename)
+
+      # save the activation as image
+      outputFeatureMap(activation, target_folder + dataset_name + "_" + filename + "_" + str(depth) + "_features.png")
+
+
+# =========================================
+# Execute experiment
+
 # read csv data
-if run_target_str is RUN_TARGET_FLOYD:
-  samples = getCSVLines("t1_reverse_data.csv")
-  samples.extend(getCSVLines("t1_udacity_data.csv"))
-else:
-  samples = getCSVLines("sample_data.csv")
+samples = getCSVLinesFromDatasets(DATA_FOLDER, DATASETS)
 
-# create generators for training and validation sets
-train_samples, validation_samples = train_test_split(samples, test_size=VALIDATION_SPLIT)
-train_generator = generator(train_samples, batch_size=BATCH_SIZE)
-validation_generator = generator(validation_samples, batch_size=BATCH_SIZE)
+# Create NVIDIA model
+model = getNVIDIAModel()
 
-# read images and measurements
-#images, measurements = readImagesAndMeasurements(samples)
+if DO_TRAIN:
+  # train model
+  history_object = train_model(model, samples)
 
-# convert images and measurements to numpy arrays, use original data
-#X_train = np.array(images)
-#y_train = np.array(measurements)
+  # save trained model
+  model.save(OUTPUT_FOLDER + 'model.h5')
+  
+  # plot the history and save as png
+  saveHistoryPlot(history_object)
 
-# construct a basic network
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D, Convolution2D, MaxPooling2D
+else: 
+  # load previous weights
+  model.load_weights('model.h5')
 
-# Simple model
-#model = Sequential()
-#model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-#model.add(Flatten())
-#model.add(Dense(1))
-
-# LeNet
-#model = Sequential()
-#model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-# TODO preprocessing layers 1x1
-#model.add(Cropping2D(cropping=((70, 25), (0, 0))))
-#model.add(Convolution2D(6, 5, 5, activation="relu"))
-#model.add(MaxPooling2D())
-#model.add(Convolution2D(6, 5, 5, activation="relu"))
-#model.add(MaxPooling2D())
-#model.add(Flatten())
-#model.add(Dense(120))
-#model.add(Dense(84))
-#model.add(Dense(1))
-
-# NVIDIA
-model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-model.add(Cropping2D(cropping=((70, 25), (0, 0))))
-model.add(Convolution2D(10, 1, 1, activation="relu")) # let the model choose the best color space
-model.add(Convolution2D(3 , 1, 1, activation="relu")) # --"--
-model.add(Convolution2D(24, 5, 5, subsample=(2, 2),  activation="relu"))
-model.add(Convolution2D(36, 5, 5, subsample=(2, 2),  activation="relu"))
-model.add(Convolution2D(48, 5, 5, subsample=(2, 2),  activation="relu"))
-model.add(Convolution2D(64, 3, 3, activation="relu"))
-model.add(Convolution2D(64, 3, 3, activation="relu"))
-model.add(Flatten())
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(10))
-model.add(Dense(1))
-
-# choose loss function and optimizer, compile the model
-model.compile(loss='mse', optimizer='adam')
-
-# fit model
-#model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
-
-# fit model with generator
-model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=5)
-
-# save the resulting model
-
-if run_target_str is RUN_TARGET_FLOYD:
-  model.save('/output/model.h5')
-else:
-  model.save('model.h5')
-
-
+# generate activation maps for images
+generateActivationMapsForImages(PREDICT_IMAGES, 5)
 
